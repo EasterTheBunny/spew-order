052e234939cd91297baf075fcf13a5b0f18d7af3
Awbrey Hughlett <athughlett@gmail.com>
2021-09-08 13:22:07 -0500

    Update Images and Landing Page
    Added new area of landing page for product features.
    Updated favicon and added branding images.
    Corrected focus border on landing page.

--- web/src/views/Landing.svelte
+++ web/src/views/Landing.svelte
@@ -1,7 +1,40 @@
+<script type="ts">
+  import LayoutGrid, { Cell } from "@smui/layout-grid/styled"
+  import Paper, { Title, Content } from "@smui/paper/styled"
+</script>
+
 <main>
-  <h1>Cipher Mountain</h1>
-  <p>Find the adventurer in you!</p>
-	<iframe src="https://cdn.forms-content.sg-form.com/35a20b74-0f35-11ec-8a80-3a9a2a8bfcfe" style="width:100%;height:600px;border:none;"/>
+  <LayoutGrid>
+    <Cell span={6}>
+      <div class="info-container">
+        <Paper color="primary">
+          <Title class="no-focus-border">Current Features</Title>
+          <Content class="info-content">
+            <dl>
+              <dt>Order Types:</dt>
+              <dd>Limit and Market</dd>
+
+              <dt>Currencies:</dt>
+              <dd>BTC, ETH, BCH</dd>
+
+              <dt>Funding:</dt>
+              <dd>BTC, ETH</dd>
+
+              <dt>Withdrawals:</dt>
+              <dd>BTC, ETH</dd>
+
+              <dt>Other:</dt>
+              <dd>Realtime market prices</dd>
+            </dl>
+            <img src="exchange-screenshot.png" alt="cryptocurrency exchange" />
+          </Content>
+        </Paper>
+      </div>
+    </Cell>
+    <Cell span={6}>
+      <iframe src="https://cdn.forms-content.sg-form.com/35a20b74-0f35-11ec-8a80-3a9a2a8bfcfe" style="width:100%;height:600px;border:none;"/>
+    </Cell>
+  </LayoutGrid>
 </main>
   
 <style>
@@ -12,16 +45,48 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
 		}
 	}
+  .info-container {
+    padding-top: 20px;
+  }
+
+  * :global(.info-content) {
+    text-align: left;
+  }
+
+  * :global(.no-focus-border:focus-visible) {
+    outline: none;
+  }
+  dl {
+    width: 100%;
+    overflow: hidden;
+    padding: 0;
+    margin: 0
+  }
+  dt {
+    float: left;
+    width: 35%;
+    /* adjust the width; make sure the total of both is 100% */
+    padding: 0;
+    margin: 0;
+    margin-right: 10px;
+    text-align: right;
+    font-weight: bold;
+  }
+  dd {
+    float: left;
+    width: 50%;
+    /* adjust the width; make sure the total of both is 100% */
+    padding: 0;
+    margin: 0
+  }
+
+  img {
+    width: 100%;
+    margin-top: 25px;
+  }
 </style>
