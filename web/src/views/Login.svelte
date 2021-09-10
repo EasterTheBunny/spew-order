4779f2978e793a40543efaaf2fc6c5e9547de910
Awbrey Hughlett <athughlett@gmail.com>
2021-09-10 12:14:16 -0500

    added pendo

--- web/src/views/Login.svelte
+++ web/src/views/Login.svelte
@@ -1,14 +1,38 @@
 <script type="ts">
+  import type { Readable } from "svelte/store"
   import { onMount } from "svelte"
   import { derived } from "svelte/store"
   import type { User } from "oidc-client"
   import { navigate } from "svelte-navigator"
   import { getOidc } from "../oidc"
 
-  const { oidc, loggedIn } = getOidc()
+  const {
+    oidc,
+    loggedIn,
+    user,
+  }: {
+    oidc: OidcService
+    loggedIn: Readable<boolean>
+    user: Readable<User>
+  } = getOidc()
 
-  const redirector = derived([loggedIn], ([$l]) => {
+  const redirector = derived([loggedIn, user], ([$l, $u]) => {
     if ($l) {
+
+      pendo.initialize({
+        visitor: {
+          id: $u.profile.sid,
+          email: $u.profile.email,
+          full_name: $u.profile.name,
+        },
+        account: {
+          id: $u.profile.sid,
+          name: $u.profile.name,
+          is_paying: true,
+          // monthly_value:// Recommended if using Pendo Feedback
+        }
+      });
+
       navigate("/", { replace: true })
     }
     return ""
