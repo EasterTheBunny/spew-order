3afa62ed1c46e04ecc66540b25a5af7e7324009e
Awbrey Hughlett <athughlett@gmail.com>
2021-11-04 11:24:31 -0500

    test

--- internal/funding/coinbase_test.go
+++ internal/funding/coinbase_test.go
@@ -7,6 +7,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/easterthebunny/spew-order/pkg/types"
+	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -62,6 +64,25 @@ version: 1`)
 		err := s.verifyRequest(pubkey, signature, []byte(body))
 		assert.NoError(t, err)
 	})
+}
+
+func TestTransactionFromBody(t *testing.T) {
+	cb := &coinbaseSource{}
+	body := `{"id":"5a6956f9-94bb-5c15-99f3-a2a690347674","type":"wallet:addresses:new-payment",` +
+		`"data":{"id":"c556eec7-53c2-576c-9158-c2eaa7c7ffca","address":"mr4xYGzGMnw5tbbu2qyBBGdKwA4ktufbPy",` +
+		`"name":null,"created_at":"2015-01-31T20:49:02Z","updated_at":"2015-01-31T20:49:02Z","resource":"address",` +
+		`"resource_path":"/v2/accounts/c334783a-e72c-5e25-bf43-e182345f28c6/addresses/c556eec7-53c2-576c-9158-c2eaa7c7ffca"},` +
+		`"delivery_attempts":1,"additional_data":{"hash":"749f267f9d238c978fe3e79a6c1f34070b0b8e5a3de8623d1bd144760bf79a5f","amount":{"amount":"0.10000000","currency":"BTC"},` +
+		`"transaction":{"id":"fe7b729d-62c6-5978-95ca-6cc1f9d23119","resource":"transaction",` +
+		`"resource_path":"/v2/accounts/c334783a-e72c-5e25-bf43-e182345f28c6/transactions/fe7b729d-62c6-5978-95ca-6cc1f9d23119"}}}`
+
+	tr, err := cb.transactionFromBody(strings.NewReader(body))
+	assert.NoError(t, err)
+
+	assert.Equal(t, types.SymbolBitcoin, tr.Symbol)
+	assert.Equal(t, "749f267f9d238c978fe3e79a6c1f34070b0b8e5a3de8623d1bd144760bf79a5f", tr.TransactionHash)
+	assert.Equal(t, "mr4xYGzGMnw5tbbu2qyBBGdKwA4ktufbPy", tr.Address)
+	assert.Equal(t, decimal.NewFromFloat(0.1).StringFixed(4), tr.Amount.StringFixed(4))
 }
 
 func TestCoinbaseGetTime(t *testing.T) {
