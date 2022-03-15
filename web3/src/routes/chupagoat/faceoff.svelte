0b86366791b2a32af02320293a380db60b43ab54
EasterTheBunny <athughlett@gmail.com>
2022-03-15 11:40:42 -0500

    updated login

---
+++ web3/src/routes/chupagoat/faceoff.svelte
@@ -0,0 +1,53 @@
+
+<script type="ts">
+  import Hero from '$lib/hero-section';
+  
+</script>
+
+<Hero style="align-items: center; justify-content: center;">
+  <div style="width: 70%; text-align: center;">
+    <div style="display: flex; justify-content: space-between;">
+      <h1>Lucky Goat</h1>
+      <div style="font-size: 1.8rem;"><i>vs</i></div>
+      <h1>Zombie Zebras</h1>
+    </div>
+    <div style="display: flex; align-items: stretch; width: 100%;">
+      <div style="display: inline;width: 75%; height: 30px; background-color: #672b7a;"></div>
+      <div style="display: inline;width: 25%; height: 30px; background-color: #ffbb00;"></div>
+    </div>
+    <div style="display: flex; justify-content: space-between;">
+      <h3>#LuckyGoatTribe x750</h3>
+      <h3>#ZombieZebras x250</h3>
+    </div>
+    <div style="display: flex; justify-content: space-between; margin-top: 20px;">
+      <div style="text-align: left;">
+        Rankings
+        <ul style="list-style: none; padding-left: 0;">
+          <li>Player 1</li>
+          <li>Player 2</li>
+          <li>Player 3</li>
+          <li>Player 4</li>
+        </ul>
+      </div>
+      <div style="text-align: center;">
+        Combined Rankings
+        <ul style="list-style: none; padding-left: 0;">
+          <li>Player 3</li>
+          <li>Player 1</li>
+          <li>Player 2</li>
+          <li>Player 4</li>
+        </ul>
+      </div>
+      <div style="text-align: right;">
+        Rankings
+        <ul style="list-style: none; padding-left: 0;">
+          <li>Player 1</li>
+          <li>Player 2</li>
+          <li>Player 3</li>
+          <li>Player 4</li>
+        </ul>
+      </div>
+    </div>
+
+  </div>
+</Hero>
--- web3/src/routes/chupagoat/install.svelte
+++ web3/src/routes/chupagoat/install.svelte
@@ -187,7 +187,7 @@
   }
 
   onMount(() => {
-    run()
+    setTimeout(run, 1000);
   })
 
 </script>
