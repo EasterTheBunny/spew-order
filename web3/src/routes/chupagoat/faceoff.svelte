03b16138e005fc194b82c9d1aff5bd89d435f388
EasterTheBunny <athughlett@gmail.com>
2022-03-15 14:00:20 -0500

    faceoff stage 1 complete

--- web3/src/routes/chupagoat/faceoff.svelte
+++ web3/src/routes/chupagoat/faceoff.svelte
@@ -1,28 +1,93 @@
+<script lang="ts" context="module">
+  import type { Load } from '@sveltejs/kit'
+  import { getProject, getMetrics } from '$lib/api'
+  import dayjs from 'dayjs'
+
+  export const load: Load = async ({ page }) => {
+    const { query } = page
+
+    let projectA: Project = null
+    let projectB: Project = null
+    let stats: any = null
+
+    const filter = {
+      projectSlugs: ["", ""],
+      metricTypes: ['HASHTAG'],
+      startTime: dayjs().subtract(1, 'day').format('YYYY-MM-DDTHH:mm:ssZ'),
+      blockSize: "1d",
+    }
+
+    if (!query.has('a') || !query.has('b')) {
+      return { status: 404, error: "projects not found" }
+    }
+
+    try {
+      projectA = await getProject(query.get('a'))
+      projectB = await getProject(query.get('b'))
+
+      filter.projectSlugs = [projectA.slug, projectB.slug]
+      const metrics = await getMetrics(filter)
+
+      stats = metrics.reduce((obj, m) => {
+        if (!obj.hasOwnProperty(m.project.slug)) {
+          obj[m.project.slug] = { tweets: m.tweetCount, mentions: m.mentionCount, users: m.userCount }
+        }
+        return obj
+      }, {})
+    } catch(e) {
+      console.log(e)
+      return { status: 404, error: "project not found" }
+    }
+
+    return { props: { projectA, projectB, stats } }
+  }
+</script>
 
 <script type="ts">
   import Hero from '$lib/hero-section';
+
+  export let projectA: Project = null;
+  export let projectB: Project = null;
+  export let stats: any = [];
   
+  const calculateScore = (tweets, mentions, users) => {
+    return Math.floor(tweets + (mentions*1.5) + (users*1.2))
+  }
+
+  const getScore = (stat) => {
+    const { tweets, mentions, users } = stat
+    return calculateScore(tweets, mentions, users)
+  }
+
+  $: projectAScore = stats.hasOwnProperty(projectA.slug) ? getScore(stats[projectA.slug]) : 0
+  $: projectBScore = stats.hasOwnProperty(projectB.slug) ? getScore(stats[projectB.slug]) : 0
+  $: sum = projectAScore + projectBScore
 </script>
 
 <Hero style="align-items: center; justify-content: center;">
   <div style="width: 70%; text-align: center;">
-    <div style="display: flex; justify-content: space-between;">
-      <h1>Lucky Goat</h1>
+    <div class="data-element">
+      <h1>{projectA.name}</h1>
       <div style="font-size: 1.8rem;"><i>vs</i></div>
-      <h1>Zombie Zebras</h1>
+      <h1>{projectB.name}</h1>
     </div>
     <div style="display: flex; align-items: stretch; width: 100%;">
-      <div style="display: inline;width: 75%; height: 30px; background-color: #672b7a;"></div>
-      <div style="display: inline;width: 25%; height: 30px; background-color: #ffbb00;"></div>
+      <div style="display: inline; width: {projectAScore/sum*100}%; height: 30px; background-color: #672b7a;"></div>
+      <div style="display: inline; width: {projectBScore/sum*100}%; height: 30px; background-color: #ffbb00;"></div>
     </div>
-    <div style="display: flex; justify-content: space-between;">
-      <h3>#LuckyGoatTribe x750</h3>
-      <h3>#ZombieZebras x250</h3>
+    <div class="data-element">
+      <h3>#{projectA.twitter?.hashtag}</h3>
+      <h3>#{projectB.twitter?.hashtag}</h3>
     </div>
-    <div style="display: flex; justify-content: space-between; margin-top: 20px;">
+    <div class="data-element">
+      <i>Score: {projectAScore}</i>
+      <i>Score: {projectBScore}</i>
+    </div>
+
+    <div class="data-element" style="margin-top: 30px; visibility: hidden;">
       <div style="text-align: left;">
         Rankings
-        <ul style="list-style: none; padding-left: 0;">
+        <ul class="ranking-list">
           <li>Player 1</li>
           <li>Player 2</li>
           <li>Player 3</li>
@@ -31,7 +96,7 @@
       </div>
       <div style="text-align: center;">
         Combined Rankings
-        <ul style="list-style: none; padding-left: 0;">
+        <ul class="ranking-list">
           <li>Player 3</li>
           <li>Player 1</li>
           <li>Player 2</li>
@@ -40,7 +105,7 @@
       </div>
       <div style="text-align: right;">
         Rankings
-        <ul style="list-style: none; padding-left: 0;">
+        <ul class="ranking-list">
           <li>Player 1</li>
           <li>Player 2</li>
           <li>Player 3</li>
@@ -51,3 +116,19 @@
 
   </div>
 </Hero>
+
+<style>
+  .data-element {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .data-element > h3 {
+    margin-bottom: 0;
+  }
+
+  .ranking-list {
+    list-style: none;
+    padding-left: 0;
+  }
+</style>
